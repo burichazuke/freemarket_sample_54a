562f6908eeac545dd99ebc0c25a2f185064e47fb
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-07 12:03:06 +0900

    ready to development
    始まる・・・！

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users,  only: [:index, :create, :show, :edit, :update]
   resources :items
+    resources :comments, only:[:create, :destroy]
   resources :categories,  only: [:index, :show]
   resources :brands,  only: [:index, :show]
 end
