79aaa0567d5f24699e218a83c2eda5fc7dc2da64
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-09 21:02:05 +0900

    ヘッダーカテゴリーのupdate
    動的変化を加えました。
    これからhoverしたものを指定して、本家に近づけていきます。

--- app/assets/javascripts/category-nav.js
+++ app/assets/javascripts/category-nav.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  //console.log(this)
+  //必ず必要
   $('#header-category').hover(
     function(){
       $('.header-nav-parent').show();
@@ -7,5 +7,28 @@ $(document).ready(function(){
     function(){
       $('.header-nav-parent').hide();
     }
+  );
+
+
+  
+  //addClassでhover箇所を指定する
+  $('#header-nav-parent').hover(
+    function(){
+      $('.header-nav-child').show();
+
+    },
+    function(){
+      $('.header-nav-child').hide();
+    } 
+  );
+
+  $('#headeer-nav-child').hover(
+    function(){
+      $('.header-nav-grand-child').show();
+
+    },
+    function(){
+      $('.header-nav-grand-child').hide();
+    } 
   );
 });
