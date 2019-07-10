ff73c935ea4a2733bfef529ee4619a8d17893a5d
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-10 17:29:27 +0900

    ヘッダー完成１！！！！！！！！！
    寺門先生に手伝ってもらいました！

--- app/assets/javascripts/category-nav.js
+++ app/assets/javascripts/category-nav.js
@@ -1,32 +1,31 @@
 $(document).ready(function(){
-  //必ず必要
-  // var parentList = $('.header-nav-parent-wrap');
-  // var parentListTrig = $('.header-nav-parent');
-  // var childList = $('.header-nav-child-wrap');
-  // var childListTrig =$('.header-nav-child');
-  // var grandchildList = $('.header-nav-grand-child-wrap');
-  // var grandchildListTrig =$('.header-nav-grand-child');
-
   //親
   $('#header-category').hover(function(){
-    $('.header-nav-parent').show();
+    $('.header-nav-parent-wrap').show();
   },function(){
-    $('.header-nav-parent').hide();
+    $('.header-nav-parent-wrap').hide();
   });
 
   //子
   $('.header-nav-parent').hover(function(){
-    
-    $(this).children('.header-nav-child-wrap').show();
+    $(this).addClass('active');
+    var children = $('.active').children('.header-nav-child-wrap');
+    children.show();
+
   },function(){
+    $(this).removeClass('active');
     $(this).children('.header-nav-child-wrap').hide();
+    
   });
 
   //孫
   $('.header-nav-child').hover(function(){
-    $(this).children('.header-nav-grand-child-wrap').show();
+    $(this).addClass('active2')
+    var grandChild = $('.active2').children('.header-nav-grand-child-wrap')
+    grandChild.show();
+
   },function(){
+    $(this).removeClass('active2');
     $(this).children('.header-nav-grand-child-wrap').hide();
   });
-  
 });
