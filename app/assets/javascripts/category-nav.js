7ed4bcca2af9f651c0be92a282bf78166981317e
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-10 15:23:21 +0900

    カテゴリーリストの編集
    一応形にはなりました。
    最初の変な挙動を直していきます。

--- app/assets/javascripts/category-nav.js
+++ app/assets/javascripts/category-nav.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
   //必ず必要
-  $('#header-category').hover(
-    function(){
-      $('.header-nav-parent').show();
-    },
-    function(){
-      $('.header-nav-parent').hide();
-    }
-  );
+  // var parentList = $('.header-nav-parent-wrap');
+  // var parentListTrig = $('.header-nav-parent');
+  // var childList = $('.header-nav-child-wrap');
+  // var childListTrig =$('.header-nav-child');
+  // var grandchildList = $('.header-nav-grand-child-wrap');
+  // var grandchildListTrig =$('.header-nav-grand-child');
 
+  //親
+  $('#header-category').hover(function(){
+    $('.header-nav-parent').show();
+  },function(){
+    $('.header-nav-parent').hide();
+  });
 
+  //子
+  $('.header-nav-parent').hover(function(){
+    
+    $(this).children('.header-nav-child-wrap').show();
+  },function(){
+    $(this).children('.header-nav-child-wrap').hide();
+  });
+
+  //孫
+  $('.header-nav-child').hover(function(){
+    $(this).children('.header-nav-grand-child-wrap').show();
+  },function(){
+    $(this).children('.header-nav-grand-child-wrap').hide();
+  });
   
-  //addClassでhover箇所を指定する
-  $('#header-nav-parent').hover(
-    function(){
-      $('.header-nav-child').show();
-
-    },
-    function(){
-      $('.header-nav-child').hide();
-    } 
-  );
-
-  $('#headeer-nav-child').hover(
-    function(){
-      $('.header-nav-grand-child').show();
-
-    },
-    function(){
-      $('.header-nav-grand-child').hide();
-    } 
-  );
 });
