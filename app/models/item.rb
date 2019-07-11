28d1edeb6369c90dc430b776340b55a1572ebe77
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-11 20:01:04 +0900

    imageモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   # belongs_to :seller, class_name: "User"
   # belongs_to :buyer, class_name: "User"
-  # has_many :images, dependent: :destroy
+  has_many :images, dependent: :destroy
   # belongs_to_active_hash :category
   # belongs_to_active_hash :brand
   # has_many :comments, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_080323) do
+ActiveRecord::Schema.define(version: 2019_07_11_104538) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_07_11_080323) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
 end
