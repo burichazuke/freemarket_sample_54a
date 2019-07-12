3fd83b452bc292700b338bf9d502adc5c3183c5d
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-12 12:54:18 +0900

    update-seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,7 +61,16 @@ Item.create([{
  }
 ])
 
-Image.create(
+Image.create([{
   image:open("public/images/m65260678049_1.jpg"),
   item_id: 1
-)
+},{
+  image:open("public/images/m78031511689_1.jpg"),
+  item_id: 2
+},{
+  image:open("public/images/m65260678049_1.jpg"),
+  item_id: 3
+},{
+  image:open("public/images/m78031511689_1.jpg"),
+  item_id: 4
+}])
