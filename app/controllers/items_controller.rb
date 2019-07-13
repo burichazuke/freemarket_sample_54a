851c3f0e9ece645c0a5bc441208e4c5276935f3b
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-13 20:37:37 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,26 @@ class ItemsController < ApplicationController
   end
 
   def buy
+    @item = Item.find(params[:id])
     render layout: "single"
+
   end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :size, :condition, :shipping_fee, :shipping_method, :prefecture, :shipping_date, :price, :status, images_attributes: [:image]).merge(seller_id: current_user.id, profit: 400)
+  end
+
+
+
 end
