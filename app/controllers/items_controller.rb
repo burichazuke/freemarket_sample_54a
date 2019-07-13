2e7279e8ff29de0d7b4d23df11be68df737a496b
miyukimaura <maumiyu0412@gmail.com>
2019-07-14 00:24:33 +0900

    更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :user_signed_in?, only:[:new, :create, :edit, :update, :destroy]
+
   def index
     @items = Item.includes(:images).order("created_at desc")
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,4 +18,14 @@ class ItemsController < ApplicationController
   def buy
     render layout: "single"
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to action: "index"
+    else
+    end
+  end
+
+  
 end
