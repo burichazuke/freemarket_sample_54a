01b1e095f0111df85d7343a9c35142cd3c77ffdb
miyukimaura <maumiyu0412@gmail.com>
2019-07-13 11:24:12 +0900

    更新

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,26 +1,22 @@
-$(document).ready(function() {
-  $('.non-active').on('click', function() {
-    $('.mypage__content__tab-content__item-list').hide('');
+$(document).on('turbolinks:load',function(){
+  $(function() {
+    $('.mypage__content__tabs > li').on('click', function(e) {
+      e.preventDefault();
+
+      console.log($(this).context.className);
+
+
+      if ($(this).context.className != "active-mypage-tab") {
+        $('.active-mypage-tab').removeClass('active-mypage-tab');
+        $(this).addClass('active-mypage-tab');
+      }
+
+      // $('.non-active-mypage-tab').addClass('active-mypage-tab');
+      // $('.non-active-mypage-tab').removeClass('non-active-mypage-tab');
+      // $('.active-mypage-tab').addClass('non-active-mypage-tab');
+      // $('.active-mypage-tab').removeClass('active-mypage-tab');
+      // $('.mypage__content__tab-content__item-list--non').show();
+      // $('.mypage__content__tab-content__item-list').hide();
+    });
   });
 });
-
-$(document).ready(function() {
-  $('.non-active').on('click', function() {
-    $('.mypage__content__tab-content__item-list--non').show('');
-  });
-});
-
-
-
-$(document).ready(function() {
-  $('.active').on('click', function() {
-    $('.mypage__content__tab-content__item-list--non').hide('');
-  });
-});
-
-$(document).ready(function() {
-  $('.active').on('click', function() {
-    $('.mypage__content__tab-content__item-list').show('');
-  });
-});
-
