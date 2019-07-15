2a764015a4da44cb810fba6aab549b21dac74b39
miyukimaura <maumiyu0412@gmail.com>
2019-07-15 14:13:39 +0900

    spec items コントローラにテストコード記述

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -14,4 +14,22 @@ describe ItemsController do
       expect(response).to render_template :index
     end
   end
+
+
+  describe 'GET #show' do
+    it "インスタンス変数を正しく取得できている" do
+      item = create(:item)
+      get :show, params: {id: item}
+      expect(assigns(:item)).to eq item
+    end
+    it "showページのビューが正しく表示できている" do
+      item = create(:item)
+      get :show, params: {id: item}
+      expect(response).to render_template :show
+    end
+  end
+## 各exampleで期待する値は1つ
+## assignsメソッドで、コントローラ内のインスタンス変数に代入されたオブジェクトを取得する、取得する場合には、インスタンス変数名から@を除いたものをキーとして渡す、呼び出しは、assigns(:item)という形式でも取り出せる。
+
+
 end
