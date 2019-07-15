66443c08809561a4275974999b2f6d57a8326580
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-15 12:08:25 +0900

    細かい修正
    インデントの調整

--- db/migrate/20190713112705_create_addresses.rb
+++ db/migrate/20190713112705_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.string :postal_code,  null: false
-      t.string :prefecture,  null: false
+      t.string :postal_code,     null: false
+      t.string :prefecture,      null: false
       t.string :municipalities,  null: false
-      t.string :address,  null: false
+      t.string :address,         null: false
       t.string :building 
-      t.references :user, foreign_key: true
+      t.references :user,        foreign_key: true
 
       t.timestamps
     end
