43e4a1c3c809e4732bcf0f14f636d7431a14481f
miyukimaura <maumiyu0412@gmail.com>
2019-07-16 20:01:13 +0900

    changed files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_060346) do
+ActiveRecord::Schema.define(version: 2019_07_15_123417) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_07_16_060346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
