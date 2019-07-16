1954b18543ea7f50ae8e1f2f506a63e30f0d7368
chizuru <czr.shinoda@gmail.com>
2019-07-16 13:13:05 +0900

    Revert "Merge branch 'master' into item-sell-view-add-data"
    This reverts commit 7fbc0daac25924c901b24bd979db5de30cb6eae7, reversing
    changes made to cde21bf38d0bdf503b5afccff3e434200a905d33.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.includes(:images).order("created_at desc")
   end
@@ -7,11 +8,28 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = ""
+    @item = Item.new
+    @item.images.build
     render layout: "single"
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path(@item)
+    else
+      render :new, layout: "single"
+    end
   end
 
   def buy
     render layout: "single"
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :size, :condition, :shipping_fee, :shipping_method, :prefecture, :shipping_date, :price, :status, :profit, images_attributes: [:image]).merge(seller_id: current_user.id)
   end
 end
