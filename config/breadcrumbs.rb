977fd12ba278b34171200bd89b3953be6d61a4b7
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-16 19:52:33 +0900

    update

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,9 +2,9 @@ crumb :root do
   link "Home", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :items do
+  link "items", items_path
+end
 
 # crumb :project do |project|
 #   link project.name, project_path(project)
