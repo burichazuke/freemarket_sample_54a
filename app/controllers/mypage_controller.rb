1f5f028c52667afe2003b140778c969259ff1c87
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-16 10:30:35 +0900

    カード情報表示

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -6,7 +6,13 @@ class MypageController < ApplicationController
   def profile
   end
 
-  def card  
+  def card
+    card = Card.find_by(user_id: current_user.id)
+    if card
+      Payjp.api_key = ENV["PAYJP_TEST_SECRET"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card = customer.cards.retrieve(card.card_id) 
+    end 
   end
 
   def logout  
