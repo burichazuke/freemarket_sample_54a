c1cf728a79e30c5cc4fe53590c5d1e6c2312d090
miyukimaura <maumiyu0412@gmail.com>
2019-07-17 20:00:05 +0900

    修正

--- db/seeds/image.rb
+++ db/seeds/image.rb
@@ -1,13 +1,13 @@
 Image.create!([{
   image:open("public/images/m65260678049_1.jpg"),
+  item_id: 1
+},{
+  image:open("public/images/m65260678049_1.jpg"),
+  item_id: 2
+},{
+  image:open("public/images/m65260678049_1.jpg"),
+  item_id: 3
+},{
+  image:open("public/images/m65260678049_1.jpg"),
   item_id: 4
-},{
-  image:open("public/images/m65260678049_1.jpg"),
-  item_id: 5
-},{
-  image:open("public/images/m65260678049_1.jpg"),
-  item_id: 6
-},{
-  image:open("public/images/m65260678049_1.jpg"),
-  item_id: 7
 }])
