9c3b43ef6a92bb22e96a4daa630651f9b045135b
chizuru <czr.shinoda@gmail.com>
2019-07-17 21:48:19 +0900

    価格部分にJSで3桁区切りを設定、注意事項のリンクを設定

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,13 +1,27 @@
 $(function(){
-  $('#price-form').on('keyup',function(){
-    var input = $(this).val();
-   
-    var fee = input * 0.1
-    $('#sellfee').html('¥' + fee)
- 
-    var profit = input * 0.9
-    $('#sellprofit').html('¥' + profit)
-    
 
+  $('#price-form').on('keyup', function(e){
+    var k = e.keyCode;
+    var input = $(this).val()
+
+    if (input.search(/^[-]?[0-9]+$([3-9][0-9][0-9]|9999999)/) == 0 ) {
+      
+      var fee = input * 0.1 
+      //Math.floor 小数点切り下げ
+      fee = String(Math.floor(fee)).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+      $('#sellfee').html("¥" + fee)
+
+      var profit = input * 0.9
+      //Math.ceil 少数点切り上げ
+      profit = String(Math.ceil(profit)).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+      $('#sellprofit').html("¥" + profit)
+
+    } else {
+      $('#sellfee').html('-')
+      $('#sellprofit').html('-')
+    }
   })
-})
+});
+
+// var input = input >= 300 && input <= 9999999
+// (300..9999999).include?this
