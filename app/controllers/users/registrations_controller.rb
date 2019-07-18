52ea8526dfe1c8fab0737a044d45d7a879c8f542
Yohei Date <itachi.0101y@gmail.com>
2019-07-18 12:22:16 +0900

    add verification method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   layout 'single'
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :set_user, only: [:sms_confirmation, :add_phone_number, :verification_code_input, :verification]
 
   # 後々、wicked用のクラスを作成してそこに基本メソッド以外のメソッドは移行する予定
   # GET /resource/sign_up
@@ -13,14 +14,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up/sms_confirmation
   def sms_confirmation
-    @user = ""
+  end
+
+  def add_phone_number
+    if @user.update(phone_number_params)
+      redirect_to :verification_code_input
+    else
+      render :sms_confirmation
+    end
+  end
+
+  def verification_code_input
+  end
+
+  def verification
     # redirect_to :address_user_registration
   end
 
   # GET /resource/sign_up/address
   def address
     @address = Address.new
-    # redirect_to :credit_card_user_registration
   end
 
   # GET /resource/sign_up/credit_card
@@ -79,12 +92,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number])
   end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    # super(resource)
     sms_confirmation_user_registration_path
   end
 
@@ -92,4 +104,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def phone_number_params
+    params.require(:user).permit(:phone_number)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :card
+  
+  VERIFICATION_CODE_LENGTH = 6
+  attr_accessor :verification_code_confirmation
+  # validates :phone_number, format: { with: /^\d{3}-?\d{4}-?\d{4}$/ }, allow_blank: true
+  after_create :issue_verification_code
+  after_create :send_verification_code
+
   with_options presence: true do
     validates :nickname 
     validates :last_name
@@ -13,5 +19,49 @@ class User < ApplicationRecord
     validates :birthday
   end
 
+  has_one :card
   has_one :address, dependent: :destroy
+
+
+# sms送信用のメソッド定義
+  def verify_and_save(attributes)
+    self.assign_attributes(attributes)
+    if self.verification_code == self.verification_code_confirmation
+      self.verified = true
+      self.verification_code = nil
+      self.save
+    else
+      self.errors.add(:verification_code_confirmation)
+      false
+    end
+  end
+
+  def self.not_verified
+    where(verified: false)
+  end
+
+  
+  private
+
+  def issue_verification_code
+    self.verification_code = VERIFICATION_CODE_LENGTH.times.map{ Random.rand(9) + 1 }.join
+    self.save!
+  end
+
+  def twilio_client
+    @twilio_client ||= Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+  end
+
+  # 080-1234-5678 => # +8180-1234-5678
+  def formatted_phone_number
+    "+81#{self.phone_number[1..-1]}"
+  end
+
+  def send_verification_code
+    twilio_client.account.sms.messages.create(
+      from: ENV["TWILIO_NUMBER"],
+      to: formatted_phone_number,
+      body: "この認証コードを入力してください。\n#{verification_code}"
+    )
+  end
 end
