6c3fe4c427556859f5a533272778b132be9c405b
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-19 11:07:52 +0900

    update

--- app/assets/javascripts/show-slide.js
+++ app/assets/javascripts/show-slide.js
@@ -1,12 +1,14 @@
 $(document).on('turbolinks:load',function(){
-  $('.item__photo__container__img').on('mouseover', function(){
-    $(this).delay(700).queue(function(){
-      $('.active-img').removeClass('active-img')
-      $(this).stop().addClass('active-img');
+  $('.item__photo__container__img').hover(function(){
+    var target = $(this)
+    setHover= setTimeout(function(){
+      $('.active-img').removeClass('active-img');
+      $(target).addClass('active-img');
       var id = $('.active-img').data("id");
-      console.log(id)
-      $('.item__photo__slide').animate({"left": id * -300 + "px", duration:'easeInOutExpo'});      
-    });
+      console.log(target)
+      $('.item__photo__slide').animate({"left": id * -300 + "px", easing:'easeInOutExpo'}); 
+    },700)
+  },function(){
+    clearTimeout(setHover)
   });
 });
-
