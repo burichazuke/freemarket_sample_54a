e9fe686e3cee33b0bc6cdf0ffd54d0cc0fca8a48
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-21 14:31:27 +0900

    routingの設定
    category.indexのルーティングを記載しました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,14 @@
 class CategoriesController < ApplicationController
   def index
     @parents = Category.all.order('id ASC').limit(13)
-    
   end
 
   def show
+    @parents = Category.find(params[:id])
   end
   
+  private
+  def Category_params
+    params.require(:category).permit(:name,:ancestry)
+  end
 end
