c191956607aeab2c8356dcfb3e70a40f0357660d
miyukimaura <maumiyu0412@gmail.com>
2019-07-23 15:17:53 +0900

    不要なコード削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    @user = User.find(params[:id])
     @item = Item.find(params[:item_id])
     @comment = @item.comments.create(commnet_params)
     if @comment.save
