697595ea3f2ced11627398938e1cb14d5ee3eaa0
miyukimaura <maumiyu0412@gmail.com>
2019-07-25 13:32:22 +0900

    jsファイル

--- app/assets/javascripts/header-mypage.js
+++ app/assets/javascripts/header-mypage.js
@@ -1,13 +1,13 @@
-// $(document).on('turbolinks:load',function(){
-//   $('.header-user-img').hover(function(){
-//     $('.mypage__nav').show();
-//   },function(){
-//     $('.mypage__nav').hide();
-//   });
-// });
+$(document).on('turbolinks:load',function(){
+  $('.header-user-img').hover(function(){
+    $('.mypage__nav').show();
+  });
+});
 
-// $(document).on('turbolinks:load',function(){
-//   $('.mypage__nav').hover(function(){
-//     $('.mypage__nav').hide();
-//   });
-// });
+$(document).on('turbolinks:load',function(){
+  $('.mypage__nav').hover(function(){
+    $('.mypage__nav').show();
+  },function(){
+    $('.mypage__nav').hide();
+  });
+});
