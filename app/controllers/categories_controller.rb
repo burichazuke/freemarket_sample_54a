83cac810f793dcb4de84a78dd021977cc986563c
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-26 16:43:23 +0900

    category/showの修正
    category/showでのCSSを修正しました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category =  Category.includes(:items).find(params[:id])
-    @children = Category.find(params[:id]).children
+    @children = Category.find(params[:id]).children.limit(8)
     @grandchildren = Category.find(params[:id]).siblings
     
     # 現在地が親なら
