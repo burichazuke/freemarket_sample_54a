7e0417caa2eada3c8948d7a1370ad7cbc5c60e2b
terakado <sakusaku.sasuke0426@gmail.com>
2019-07-28 21:52:21 +0900

    update

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,20 +1,18 @@
 FactoryBot.define do
 
-  price = Faker::Number.within(300..9999999)
-
   factory :item do
     association :category
     association :seller, factory: :user
-    name              { Faker::Name.name }
-    description       {"説明"}
+    name              { Faker::Games::Pokemon.name }
+    description       {"#{Faker::Games::Pokemon.name}大好きです"}
     size              {"s"}
     condition         {"新品"}
     shipping_fee      {"送料込み"}
     shipping_method   {"らくらくメルカリ便"}
     prefecture        {"東京都"}
     shipping_date     {"二日"}
-    price             { price }
-    profit            { price * 0.9 }
+    price             { 1000 }
+    profit            { 900 }
     status            {0}
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 describe Item do
   describe '#create' do
 
-    it "商品名が空でも出品できるか" do
+    it "商品名が空なら出品できない" do
       item = build(:item, name: nil)
       item.valid?
       expect(item.errors[:name]).to include("を入力してください")
     end
 
-    it "商品説明が空でも出品できるか" do
+    it "商品説明が空なら出品できない" do
       item = build(:item, description: nil)
       item.valid?
       expect(item.errors[:description]).to include('を入力してください')
