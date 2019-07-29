4ab094ad75a4a592bc34f78b4c00d1cd0f8330b6
chizuru <czr.shinoda@gmail.com>
2019-07-29 17:01:43 +0900

    turbolinks削除(複数回いいねが付く現象を回避)、ヘッダーのハートの表示が変わらないように修正

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -1,45 +1,39 @@
-$(document).on("turbolinks:load",function() {
+$(document).on('click', "#item__button", function(e) {
+  e.preventDefault();
+  var id = $(this).data("item-id")
 
-  $(document).on('click', "#item__button", function(e) {
-    e.preventDefault();
-    var id = $(this).data("item-id")
-    var count = $(this).data("favo-id")
-    console.log(id)
-    $.ajax({
-      url: /favorites/,
-      type: 'POST',
-      data: { item_id: id},
-      dataType: 'json',
-    })
-
-    .done(function(data){
-      $('.fa-heart-o').addClass('fa-heart')
-      $('.fa-heart-o').removeClass('fa-heart-o')
-      $('#item__button').attr('id', 'favorited-button')
-      num = Number($('.xxx').text()) + 1
-      $('.xxx').text(num)
-    })
+  $.ajax({
+    url: '/favorites',
+    type: 'POST',
+    data: { item_id: id},
+    dataType: 'json',
   })
 
-  $(document).on('click', "#favorited-button", function(e) {
-    e.preventDefault();
-    var id = $(this).data("item-id")
-    var count = $(this).data("favo-id")
-    console.log(id)
-    $.ajax({
-      url: '/favorites/:id',
-      type: 'DELETE',
-      data: { item_id: id },
-      dataType: 'json',
-    })
+  .done(function(data){
+    $('.favorite.fa-heart-o').addClass('fa-heart')
+    $('.favorite.fa-heart-o').removeClass('fa-heart-o')
+    $('#item__button').attr('id', 'favorited-button')
+    num = Number($('.favorite_count').text()) + 1;
+    $('.favorite_count').text(num)
+  })
+})
 
-    .done(function(data){
-      $('.fa-heart').addClass('fa-heart-o')
-      $('.fa-heart').removeClass('fa-heart')
-      $('#favorited-button').attr("id", "item__button")
-      $('#favorited-button').attr("id", "item__button")
-      num = Number($('.xxx').text()) - 1
-      $('.xxx').text(num)
-    })
+$(document).on('click', "#favorited-button", function(e) {
+  e.preventDefault();
+  var id = $(this).data("item-id")
+  
+  $.ajax({
+    url: '/favorites',
+    type: 'DELETE',
+    data: { item_id: id },
+    dataType: 'json',
+  })
+
+  .done(function(data){
+    $('.favorite.fa-heart').addClass('fa-heart-o')
+    $('.favorite.fa-heart').removeClass('fa-heart')
+    $('#favorited-button').attr("id", "item__button")
+    num = Number($('.favorite_count').text()) - 1;
+    $('.favorite_count').text(num)
   })
 })
