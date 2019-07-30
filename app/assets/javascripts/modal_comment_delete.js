5da231cbdb4863bee3bc1b41b14792f9439b3ee5
miyukimaura <maumiyu0412@gmail.com>
2019-07-30 14:15:18 +0900

    訂正

--- app/assets/javascripts/modal_comment_delete.js
+++ app/assets/javascripts/modal_comment_delete.js
@@ -2,7 +2,6 @@ $(document).on('turbolinks:load',function(){
   $('.delete-icon').on('click',function(e){
       e.preventDefault();
       var dataId = $(this).data('id');
-      console.log(dataId)
       $(`#${dataId}`).fadeIn("slow");
   });
 });
