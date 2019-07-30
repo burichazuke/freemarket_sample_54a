9ba9de5e4c23abfbe47b2b7ef62bcdb560786baf
miyukimaura <maumiyu0412@gmail.com>
2019-07-30 13:49:39 +0900

    controller　不要な記述削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-  before_action :correct_user, only: [:destroy]
-
   def create
     @item = Item.find(params[:item_id])
     @comment = @item.comments.create(commnet_params)
@@ -24,13 +22,6 @@ class CommentsController < ApplicationController
   
   def commnet_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
-  end
-
-  def correct_user
-    @comment = current_user.comments.find_by(id: params[:id])
-    unless @comment
-      redirect_to item_path
-    end
   end
 
 end
