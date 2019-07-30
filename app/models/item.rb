810f14fe7f4069c2bcd36a54dc1cfe15899a2a10
Yohei Date <itachi.0101y@gmail.com>
2019-07-30 20:28:58 +0900

    修正しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,33 +1,27 @@
 class Item < ApplicationRecord
   before_save :item_profit
-  attr_accessor :image_validation, :image_files, :delete_image_files, :parent_id, :child_id, 
+  attr_accessor :image_validation, :image_files, :delete_image_files, :parent_id, :child_id
+  
+  has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :category
+  accepts_nested_attributes_for :images
+  # belongs_to_active_hash :category
+  # belongs_to_active_hash :brand
+
+  validates :image_validation, :name, :description, :price, :size, :condition, :shipping_fee, :shipping_method, :prefecture, :shipping_date, presence: true
 
   def item_profit
     self.profit = price * 0.9
   end
 
-  belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User", optional: true
-  has_many :images, dependent: :destroy
-  belongs_to :category
-  accepts_nested_attributes_for :images
-  # belongs_to_active_hash :category
-
-  # belongs_to_active_hash :brand
-
-  validates :name, :description, :price, :images, presence: true
-  validates :size, :condition, :shipping_fee, :shipping_method, :prefecture, :shipping_date, presence: true
-
-  has_many :favorites, dependent: :destroy
-  has_many :users, through: :favorites
-
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-
-  has_many :comments, dependent: :destroy
-
-  validates :image_validation, :name, :description, :price, :size, :condition, :shipping_fee, :shipping_method, :prefecture, :shipping_date, presence: true
 end
 
 
