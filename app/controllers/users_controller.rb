a47519cd0eb2886971b2f6a149729c10baf21aea
miyukimaura <maumiyu0412@gmail.com>
2019-07-31 14:54:19 +0900

    他の商品表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = User.find(params[:id])
-
+    @item = Item.where(params[:id])
+    @items = Item.where(params[:id])
+    @user_items = Item.where(seller_id: @item.seller_id).order('created_at DESC').limit(6)
   end
 
   def edit
