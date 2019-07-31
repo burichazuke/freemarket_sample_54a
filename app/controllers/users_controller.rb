f6c4e354f198ac84991c1afc47cd4f53a27861c5
miyukimaura <maumiyu0412@gmail.com>
2019-07-31 16:28:49 +0900

    users_controller　修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @item = Item.where(params[:id])
-    @items = Item.where(params[:id])
-    @user_items = Item.where(seller_id: @item.seller_id).order('created_at DESC').limit(6)
+    @user_items = Item.where(seller_id: @user.id).order('created_at DESC').limit(6)
   end
 
   def edit
