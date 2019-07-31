ddbb435112008a4a95e1319cd51070f2533950db
TaikiKamaka <keeponrockin.kamachan9@gmail.com>
2019-07-31 14:52:40 +0900

    細かい修正

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,11 +1,11 @@
 module ItemsHelper
   def next_item
-    next_item = Item.all.order('created_at DESC').limit(1).first
+    next_item = Item.last
   end
 
   def prev_item
     prev_item = Item.all.sample
-    prev_item_id =prev_item
+
   end
 
 end
