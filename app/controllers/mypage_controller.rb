08618e454593d88648008105037a95f25839c027
Yohei Date <itachi.0101y@gmail.com>
2019-07-31 17:21:32 +0900

    update

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -27,6 +27,10 @@ class MypageController < ApplicationController
 
   def profile
   end
+  
+  def like
+    @items = current_user.favorite_items
+  end
 
   def update_profile
     if current_user.update(user_profile_params)
