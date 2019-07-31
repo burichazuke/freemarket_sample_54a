ac98123933d86a50ceb21db93018e10032685921
miyukimaura <maumiyu0412@gmail.com>
2019-07-31 15:24:29 +0900

    comments_controller　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-    @comment = @item.comments.create(commnet_params)
+    @comment = @item.comments.new(commnet_params)
     if @comment.save
       redirect_to item_path(@item)
     end
@@ -19,6 +19,7 @@ class CommentsController < ApplicationController
     end
   end
 
+  private
   
   def commnet_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
